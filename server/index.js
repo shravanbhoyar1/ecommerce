5f820492167cdfb63c6fb4d787cdccfb7eb7c3e8
shravanbhoyar1 <shravanbhoyar3507>
2025-02-06 11:38:29 +0530

    database connection establish

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 import express from 'express'
-
+import cors from 'cors'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
+app.use(cors());
 
-const PORT = 5000;
+
+const connectDB = async ()=>{
+    const con = await mongoose.connect(process.env.MONGO_URI);
+    if(con)
+    {
+        console.log(`MongoDb Connection Establish..`);
+    }
+
+}
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
+    connectDB();
 })
