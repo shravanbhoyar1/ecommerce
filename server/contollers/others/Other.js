0fe7628e513826c08025020b84a96e61ce4fa66c
shravanbhoyar1 <shravanbhoyar3507>
2025-02-06 21:46:55 +0530

    api intergration

---
+++ server/contollers/others/Other.js
@@ -0,0 +1,15 @@
+const getHealth = (req,res)=>{
+    res.status(200).json({
+       success:true,
+       message:"server is running"
+    });
+   }
+
+const isError  = (req,res)=>{
+    res.status(400).json({
+        success:false,
+        message:"url is incorrect"
+    });
+}   
+
+   export {getHealth, isError}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import { getHealth, isError } from './contollers/others/Other.js'
+
 dotenv.config();
 const app = express();
+app.use(express.json());
 app.use(cors());
-
 
 const connectDB = async ()=>{
     const con = await mongoose.connect(process.env.MONGO_URI);
@@ -15,6 +17,58 @@ const connectDB = async ()=>{
     }
 
 }
+
+app.get("/health",getHealth);
+
+app.post("/signup", async (req,res)=>{
+   const {name,email,phone,address,password,rePassword} = req.body;
+
+   if(!password)
+   {
+    res.status(400).json({
+        success:false,
+        message:"Password is Required"
+    });
+   }
+   if(password!==rePassword)
+   {
+    res.status(400).json({
+        success:false,
+        message:"Password does not match"
+    });
+   }
+   if(!name)
+   {
+    res.status(400).json({
+        success:false,
+        message:"Name is Required"
+    });
+   }
+   if(!email)
+   {
+    res.status(400).json({
+        success:false,
+        message:"Email is Required"
+    });
+   }
+   if(!phone)
+   {
+    res.status(400).json({
+        success:false,
+        message:"Phone is Required"
+    });
+   }
+   if(!address)
+   {
+    res.status(400).json({
+        success:false,
+        message:"Address is Required"
+    });
+   }
+});
+
+app.get("*",isError);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
