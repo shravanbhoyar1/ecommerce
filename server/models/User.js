8e8fd67e34ffe4188626d4e954abf176682412af
shravanbhoyar1 <shravanbhoyar3507>
2025-02-06 18:42:58 +0530

    product and order model added

--- server/models/User.js
+++ server/models/User.js
@@ -2,26 +2,26 @@ import {model,Schema} from 'mongoose'
 
 const userSchema  = new Schema({
     name:{
-        type:"String",
+        type:String,
         required:true,
     },
     email:{
-        type:"String",
+        type:String,
         unique:true,
     },
     phone:{
-        type:"String",
+        type:String,
         unique:true,
     },
     address:{
-        type:"String",
+        type:String,
     },
     role:{
-        type:"String",
+        type:String,
         default:"user",
     },
     password:{
-        type:"String",
+        type:String,
         required:true,
     }, 
 },
