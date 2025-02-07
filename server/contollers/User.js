40bf0e12bece279c8193e511671380e52a6a6f14
shravanbhoyar1 <shravanbhoyar3507>
2025-02-07 11:52:20 +0530

    jwt token concept

--- server/contollers/User.js
+++ server/contollers/User.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from './../models/User.js'
+import jwt from 'jsonwebtoken'
 const signUp = async (req, res) => {
     const { name, email, phone, address, password, rePassword } = req.body;
 
@@ -70,22 +71,30 @@ const login = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return  res.status(400).json({
-        success: false, message: "Email and Password is required"});
+        return res.status(400).json({
+            success: false, message: "Email and Password is required"
+        });
     }
-   
-    const user = await User.findOne({email});
-    if(!user)
-    {
-       return res.status(400).json({success:false, message:"Please signup first before logging in"});
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ success: false, message: "Please signup first before logging in" });
     }
 
     const isPasswordMatch = bcrypt.compareSync(password, user.password);
 
-    if(isPasswordMatch ){
-        return res.status(200).json({success:true, message:"Login successfully"});
-    }else{
-        return res.status(400).json({success:false, message:"Invalid credentiala"});
+    if (isPasswordMatch) {
+        const jwtToken = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+        res.setHeader("Authorization", `Bearer ${jwtToken}`);
+
+        return res.status(200).json({
+            success: true,
+            token: jwtToken,
+            message: "Login successfully"
+        });
+    }
+    else {
+        return res.status(400).json({ success: false, message: "Invalid credentiala" });
     }
 }
 
